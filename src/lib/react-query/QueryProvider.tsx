739ad7f9625bba604ce375cba800f4846c6b53cb
Jordi van Cuijlenborg <jordivcb@icloud.com>
2024-12-07 03:52:03 +0100

    fix: hide react query dev tools if env variable is set to false

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+const showReactDevTools = import.meta.env.VITE_SHOW_REACT_DEV_TOOLS;
 const queryClient = new QueryClient();
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  if (!showReactDevTools) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        {children}
+        {showReactDevTools && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+    );
+  } else {
+    return (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    );
+  }
 };
